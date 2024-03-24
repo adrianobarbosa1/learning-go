7b3b25f0a9052e9263bea81b7f0cf277f695582a
Adriano Barbosa <adriano.barbosa100@gmail.com>
2024-03-24 13:38:44 -0300

    feat: change name to main

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # learning-go
+
+# Frequently Asked Questions (FAQ)
+
+https://go.dev/doc/faq#creating_a_new_language
+
+# Link de exercicios
+
+https://github.com/vkorbes/aprendago/issues?q=is%3Aopen+is%3Aissue+label%3Aexerc%C3%ADcio
---
+++ variaveis-valores-e-tipos/02-hello-world/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Printf("Hello world!")
+}
+
+// ### Hello world!
+
+// - Estrutura básica:
+//   - package main.
+//   - func main: é aqui que tudo começa, é aqui que tudo acaba.
+//   - import.
+// - Packages:
+//   - Pacotes são coleções de funções pré-prontas (ou não) que você pode utilizar.
+//   - Notação: pacote.Identificador. Exemplo: fmt.Println()
+//   - Documentação: fmt.Println.
+// - Variáveis: "uma variável é um objeto (uma posição na memória) capaz de reter e representar um valor ou expressão."
+// - Variáveis não utilizadas? Não pode: \_ nelas.
+// - ...funções variádicas.
+// - Lição principal: package main, func main, pacote.Identificador.
